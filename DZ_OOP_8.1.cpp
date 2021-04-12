d89c90283c480f42f3a7b174d30c871dfde8c88d
CatherineGlv <cool_kiti@mail.ru>
2021-04-12 17:18:54 +1000

    Start

---
+++ DZ_OOP_8.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+class MyException {
+private:
+	char message[256]
+public:
+	const char* get() {
+	return message
+	}
+};
+double divide(int a, int b) {
+	if (b == 0) {
+		throw MyException("Division by zero"); // проблема с этим
+	}
+	return a / b
+}
+
+int main()
+{
+	cout << "Enter a number 1: "
+	int c;
+	cin >> c;
+	cout << "Enter a number 2: ";
+	int d;
+	cin >> d;
+	try
+	{
+		double x = divide(c, d)
+	}
+	catch (MyException& e) {
+		cout << e.get()
+		throw e;
+	}
+}
---
+++ DZ_OOP_8.2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std
+class Ex
+{
+public:
+    double n;
+    Ex(const double& n_) : n(n_) {}
+    ~Ex() {}
+};
+
+class Bar
+{private:
+    double y
+public:
+    Bar() 
+    { 
+        y = 0
+    }
+    ~Bar() {}
+    void set(const double& a)
+    {
+        if (y + a > 100)
+        {
+            y = a;
+
+        }
+        else throw Ex(a * y)
+    }
+};
+
+int main()
+{
+    Bar b
+    int n
+    while (cin >> n && n != 0) 
+    {try
+        {
+            b.set(n)
+        }
+        catch (Ex& err)
+        {
+            cerr << "Exception: ay " << err.n << endl;
+        }
+    }
+}
